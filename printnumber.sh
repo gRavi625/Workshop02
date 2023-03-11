5c7d94a4a991ae314ece97fc33c8dbdf065f3d2d
Ravi Kumar <gravi625@gmail.com>
2023-03-11 09:58:29 +0530

    Number is printing after skipping

--- printnumber.sh
+++ printnumber.sh
@@ -1,5 +1,9 @@
-for (( i=1; i<=10; i++ ))
-do
-   echo -n "$i "
-done
-echo ""
+echo "Welcome to Workshop 2"
+
+declare -A selected_numbers
+for i in {1..10}; do
+   if [ $i -eq 1 ] || [ $i -eq 2 ] || [ $i -eq 4 ] || [ $i -eq 7 ] || [ $i -eq 9 ] || [ $i -eq 10 ]; then
+      selected_numbers[$i]=$i
+    fi
+   done
+   echo "${selected_numbers[@]}"
